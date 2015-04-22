349899950b9080e3f99313f7ca0774ce3ad1615a
Garret Raziel <boloomka@gmail.com>
2015-04-22 23:17:12 +0200

    changes in repo list presentation

--- ghactivity/static/ghactivity/repo_list.js
+++ ghactivity/static/ghactivity/repo_list.js
@@ -7,11 +7,8 @@ $(document).ready(function () {
             for (var i = 0; i < response.length; i++) {
                 var element = $("#" + response[i].id);
                 if (response[i].status && element.hasClass("disabled")) {
-                    var result_html = '<a href="' + response[i].url + '">' + response[i].name + '</a>';
-                    element.html(result_html);
                     element.removeClass("disabled");
                 } else if (!response[i].status && !element.hasClass("disabled")) {
-                    element.html(response[i].name);
                     element.addClass("disabled");
                 }
             }
