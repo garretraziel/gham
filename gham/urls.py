69df60a431e402a50fb09058958e0ddd59fc6668
Garret Raziel <boloomka@gmail.com>
2015-05-04 22:35:17 +0200

    add "about" pages

--- gham/urls.py
+++ gham/urls.py
@@ -7,9 +7,8 @@ from .views import index, logout_user
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^logout/$', logout_user, name='logout'),
-    url(r'^model/$', TemplateView.as_view(template_name="model.html"), name='model'),
-    url(r'^dataset/$', TemplateView.as_view(template_name="dataset.html"), name='dataset'),
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
+    url(r'^dataset/$', TemplateView.as_view(template_name="dataset.html"), name='dataset'),
 
     url(r'^activity/', include('ghactivity.urls')),
     url(r'^admin/', include(admin.site.urls)),
